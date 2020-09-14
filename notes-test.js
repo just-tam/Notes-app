abeaf124f8a5302835b5b29688f3ab46a90150b6
Tamara Jeffrey <just-tam@live.com>
2020-09-14 15:35:17 +0100

    testing for viewing notes

--- notes-test.js
+++ notes-test.js
@@ -4,3 +4,11 @@ function textCanBePushedIntoNotes() {
     notes.addNote(text);
     assert.isTrue(notes.notesList.length === 1);
 }
+
+function canReturnATextNote() {
+    var notes = new Notes();
+    var text = "I'm testing with Javascript!"
+    notes.addNote(text);
+    notes.viewNotes();
+    assert.isTrue(notes.viewNotes() === "I'm testing with Javascript!")
+}
--- notes.js
+++ notes.js
@@ -6,4 +6,10 @@ class Notes {
     addNote(text) {
         this.notesList.push(text);
     }
+
+    viewNotes() {
+        for (var i = 0; i < this.notesList.length; i++) {
+            return this.notesList[i];
+        }
+    }
 }
